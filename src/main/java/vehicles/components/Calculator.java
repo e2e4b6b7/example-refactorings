53f269e3c4f79bcff3d0b0616b07f4899c8f175b
Roman Venediktov <e2e4b6b7@gmail.com>
2021-05-18 02:11:02 +0300

    Some refactorings

--- src/main/java/vehicles/components/Calculator.java
+++ src/main/java/vehicles/components/Calculator.java
@@ -1,12 +1,12 @@
 package main.java.vehicles.components;
 
-import main.java.vehicles.components.vehicleComponents.EngineImpl;
-
 public class Calculator {
     public static int numberOfCoconuts = 10;
 
     public static Integer doCalculations() {
-        Integer temp = EngineImpl.getInteger();
+        Integer temp1 = 10;
+        temp1 *= 3;
+        Integer temp = temp1;
         return incrementWithA(temp, 2);
     }
 
